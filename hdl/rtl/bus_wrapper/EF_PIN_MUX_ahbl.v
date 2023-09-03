4611bebb29e46ccf81d8025a85a99f3e99894a82
Mohamed Shalan <mshalan@aucegypt.edu>
2023-09-03 15:07:16 +0300

    Regenerating the wrapper and the header

--- hdl/rtl/bus_wrapper/EF_PIN_MUX_ahbl.v
+++ hdl/rtl/bus_wrapper/EF_PIN_MUX_ahbl.v
@@ -31,9 +31,9 @@ module EF_PIN_MUX_ahbl (
 	input	wire [31:0]	io_in,
 	output	wire [31:0]	io_out,
 	output	wire [31:0]	io_oeb,
-	output	wire [126:0]	p_in,
-	input	wire [126:0]	p_out,
-	input	wire [126:0]	p_oeb,
+	output	wire [127:0]	p_in,
+	input	wire [127:0]	p_out,
+	input	wire [127:0]	p_oeb,
 	input	wire 		HCLK,
 	input	wire 		HRESETn,
 	input	wire [31:0]	HADDR,
@@ -47,7 +47,7 @@ module EF_PIN_MUX_ahbl (
 	output	wire 		HREADYOUT
 );
 	localparam[15:0] FN_SEL0_REG_ADDR = 16'h0000;
-	localparam[15:0] FN_SEL0_REG_ADDR = 16'h0004;
+	localparam[15:0] FN_SEL1_REG_ADDR = 16'h0004;
 
 	reg             last_HSEL;
 	reg [31:0]      last_HADDR;
@@ -63,7 +63,11 @@ module EF_PIN_MUX_ahbl (
 		end
 	end
 
+	reg	[31:0]	FN_SEL0_REG;
+	reg	[31:0]	FN_SEL1_REG;
 
+	wire[31:0]	sel0	= FN_SEL0_REG[31:0];
+	wire[31:0]	sel1	= FN_SEL1_REG[31:0];
 	wire		ahbl_valid	= last_HSEL & last_HTRANS[1];
 	wire		ahbl_we	= last_HWRITE & ahbl_valid;
 	wire		ahbl_re	= ~last_HWRITE & ahbl_valid;
@@ -81,7 +85,11 @@ module EF_PIN_MUX_ahbl (
 		.sel1(sel1)
 	);
 
+	`AHB_REG(FN_SEL0_REG, 0)
+	`AHB_REG(FN_SEL1_REG, 0)
 	assign	HRDATA = 
+			(last_HADDR == FN_SEL0_REG_ADDR) ? FN_SEL0_REG :
+			(last_HADDR == FN_SEL1_REG_ADDR) ? FN_SEL1_REG :
 			32'hDEADBEEF;
 
 
