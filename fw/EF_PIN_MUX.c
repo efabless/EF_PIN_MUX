340be16bb4c535eb4a355e818bbd8bd6669d81c1
NouranAbdelaziz <nouran.abdelaziz@aucegypt.edu>
2024-06-11 10:39:59 +0300

    update fw to have 48 gpios

--- fw/EF_PIN_MUX.c
+++ fw/EF_PIN_MUX.c
@@ -14,6 +14,12 @@ void EF_PinMux_writeSelect2(uint32_t pin_mux_base, int select){
     pin_mux->fn_sel1 = select ;
 }
 
+void EF_PinMux_writeSelect3(uint32_t pin_mux_base, int select){
+
+    EF_PIN_MUX_TYPE* pin_mux = (EF_PIN_MUX_TYPE*)pin_mux_base;
+    pin_mux->fn_sel2 = select ;
+}
+
 int EF_PinMux_readSelect(uint32_t pin_mux_base){
 
     EF_PIN_MUX_TYPE* pin_mux = (EF_PIN_MUX_TYPE*)pin_mux_base;
@@ -23,6 +29,11 @@ int EF_PinMux_readSelect2(uint32_t pin_mux_base){
 
     EF_PIN_MUX_TYPE* pin_mux = (EF_PIN_MUX_TYPE*)pin_mux_base;
     return (pin_mux->fn_sel1);
+}
+int EF_PinMux_readSelect3(uint32_t pin_mux_base){
+
+    EF_PIN_MUX_TYPE* pin_mux = (EF_PIN_MUX_TYPE*)pin_mux_base;
+    return (pin_mux->fn_sel2);
 }
 
 void EF_PinMux_writeSelectGpio1(uint32_t pin_mux_base, enum gpio_select gpio, enum select_types select){
@@ -41,9 +52,20 @@ void EF_PinMux_writeSelectGpio2(uint32_t pin_mux_base, enum gpio_select gpio, en
     EF_PinMux_writeSelect2(pin_mux_base, select_val);
 }
 
+void EF_PinMux_writeSelectGpio3(uint32_t pin_mux_base, enum gpio_select gpio, enum select_types select){
+
+    int select_val = EF_PinMux_readSelect3(pin_mux_base);
+    select_val &= ~(0x3 << gpio); // zero the needed bits
+    select_val |= select << gpio;
+    EF_PinMux_writeSelect3(pin_mux_base, select_val);
+}
+
 void EF_PinMux_writeSelectGpio(uint32_t pin_mux_base, enum gpio_select gpio, enum select_types select){
 
-    if (gpio > 30) {
+    if (gpio > 62){
+        EF_PinMux_writeSelectGpio3(pin_mux_base, gpio - 32, select);
+    }
+    else if (gpio > 30 && gpio <= 62) {
         EF_PinMux_writeSelectGpio2(pin_mux_base, gpio - 32, select);
     }else{
         EF_PinMux_writeSelectGpio1(pin_mux_base, gpio, select);
--- fw/EF_PIN_MUX.h
+++ fw/EF_PIN_MUX.h
@@ -7,19 +7,30 @@
 enum select_types{F0 = 0, F1 = 1, F2 = 2, F3 = 3};
 
 // specific to cheetah pin assignment not generic
-enum gpio_select{GPIO0 = 0, GPIO1 = 2, GPIO2 = 4, GPIO3 = 6, GPIO4 = 8, GPIO5 = 10, GPIO6 = 12, GPIO7 = 14, GPIO8 = 16, GPIO9 = 18, GPIO10 = 20, GPIO11 = 22, GPIO12 = 24, GPIO13 = 26, GPIO14 = 28, GPIO24 = 30, GPIO26 = 32, GPIO27 = 34, GPIO28 = 36, GPIO29 = 38, GPIO30 = 40};
+enum gpio_select{GPIO8 = 0, GPIO9 = 2, GPIO10 = 4, GPIO11 = 6, GPIO12 = 8, GPIO13 = 10, GPIO14 = 12, GPIO15 = 14, 
+                GPIO16 = 16, GPIO17 = 18, GPIO18 = 20, GPIO19 = 22, GPIO20 = 24, GPIO21 = 26, GPIO22 = 28, GPIO23 = 30, 
+                GPIO24 = 32, GPIO25 = 34, GPIO26 = 36, GPIO27 = 38, GPIO28 = 40, GPIO29 = 42, GPIO30 = 44, GPIO31 = 46,
+                GPIO32 = 48, GPIO33 = 50, GPIO34 = 52, GPIO35 = 54, GPIO36 = 56, GPIO37 = 58, GPIO38 = 60, GPIO39 = 62,
+                GPIO40 = 64, GPIO41 = 66, GPIO42 = 68, GPIO43 = 70, GPIO44 = 72, GPIO45 = 74, GPIO46 = 76, GPIO47 = 78,
+                GPIO40 = 80, GPIO41 = 82, GPIO42 = 84, GPIO43 = 86, GPIO44 = 88, GPIO45 = 90, GPIO46 = 92, GPIO47 = 94};
 
 void EF_PinMux_writeSelect(uint32_t pin_mux_base, int select);
 
 void EF_PinMux_writeSelect2(uint32_t pin_mux_base, int select);
 
+void EF_PinMux_writeSelect3(uint32_t pin_mux_base, int select);
+
 int EF_PinMux_readSelect(uint32_t pin_mux_base);
 
 int EF_PinMux_readSelect2(uint32_t pin_mux_base);
 
+int EF_PinMux_readSelect3(uint32_t pin_mux_base);
+
 void EF_PinMux_writeSelectGpio1(uint32_t pin_mux_base, enum gpio_select gpio, enum select_types select);
 
 void EF_PinMux_writeSelectGpio2(uint32_t pin_mux_base, enum gpio_select gpio, enum select_types select);
+
+void EF_PinMux_writeSelectGpio3(uint32_t pin_mux_base, enum gpio_select gpio, enum select_types select);
 
 void EF_PinMux_writeSelectGpio(uint32_t pin_mux_base, enum gpio_select gpio, enum select_types select);
 
